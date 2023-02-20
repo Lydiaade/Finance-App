f5322b2f1e334e09fa83c62263277bef5ccb103d
Lydiaade <lydiaadejumo@outlook.com>
2023-02-20 08:27:47 +0000

    Restructure frontend docs

--- fm-ui/src/App.js
+++ fm-ui/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Finance Manager
         </a>
       </header>
     </div>
