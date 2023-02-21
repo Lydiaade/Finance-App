164f9013043184533083082089e715b03287a6b9
Lydiaade <lydiaadejumo@outlook.com>
2023-02-21 16:38:53 +0000

    Connect front and and backend locally

---
+++ fm-backend/src/main/java/com/controller/FinanceManagerController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import com.service.FinanceManagerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RestController
+public class FinanceManagerController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> status_check(){
+        return new ResponseEntity<>("Finance Manager is running", HttpStatus.OK);
+    }
+
+    @GetMapping("/salary")
+    public ResponseEntity<Integer> current_salary(){
+        FinanceManagerService service = new FinanceManagerService();
+        return new ResponseEntity<>(service.getCurrentSalary(), HttpStatus.OK);
+    }
+}
