a334cb0c09525d6fbba98de9e3b689db70f03976
Lydiaade <lydiaadejumo@outlook.com>
2023-05-29 19:26:29 +0100

    [FM-13] Fix for reading segment in ui and loading segment into backend

--- fm-ui/src/components/SegmentContainer.jsx
+++ fm-ui/src/components/SegmentContainer.jsx
@@ -9,14 +9,12 @@ class SegmentContainer extends Component {
             <table className="table container-fluid">
                 <thead>
                 <tr className="segment-header">
-                    <th scope="col" className="ID">ID</th>
-                    <th scope="col" className="Segment">Segment</th>
+                    <th scope="col" className="Segment">Segment Name</th>
                 </tr>
                 </thead>
                 <tbody>
                 {segments.map((segment) => (
                     <tr className="segment" key={segments.indexOf(segment)}>
-                        <th scope="row" className="segmentID" id={segment.id}>{segment.id}</th>
                         <td className="segmentName">{segment.name}</td>
                     </tr>
                 ))}
