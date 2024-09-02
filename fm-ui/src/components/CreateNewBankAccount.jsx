c6ae050cc71ce2659fefb6c693740b053007314b
Lydia Adejumo <lydiaadejumo@outlook.com>
2024-09-02 04:55:24 +0100

    Reformat form layout

--- fm-ui/src/components/CreateNewBankAccount.jsx
+++ fm-ui/src/components/CreateNewBankAccount.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {BACKEND_URL} from "../config";
 import "./CreateNewBankAccount.css"
+import {Button, Col, Form, FormGroup, Row} from "react-bootstrap";
 
 class CurrentAccountOverview extends Component {
     state = {
@@ -50,6 +51,7 @@ class CurrentAccountOverview extends Component {
     }
 
     handleSubmit(event) {
+        event.preventDefault();
         let payload = {
             "name": this.state.account.name,
             "accountType": this.state.account.accountType,
@@ -177,15 +179,6 @@ class CurrentAccountOverview extends Component {
         }))
     }
 
-    setCurrency(event) {
-        this.setState(prevState => ({
-            account: {
-                ...prevState.account,
-                currency: event.target.value
-            }
-        }))
-    }
-
     setIsMainAccount(event) {
         this.setState(prevState => ({
             account: {
@@ -200,77 +193,96 @@ class CurrentAccountOverview extends Component {
         return (
             <div className="new-account-form container">
                 <h1>Account Details</h1>
-                <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-                    <div className="form-group">
-                        <label>Account Name:</label>
-                        <input type="text" className="form-control" name="accountName" value={this.state.account.name}
-                               onChange={(e) => this.setAccountName(e)}/>
-
-                    </div>
-                    <div className="form-group">
-                        <label>Account Type:</label>
-                        <select className="form-control" value={this.state.account.accountType}
-                                onChange={this.handleAccountTypeChange}>
-                            <option value="">Please select account type</option>
-                            {this.state.accountTypes.map((type) => (
-                                <option value={type} key={type}>{type}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label>Sort Code:</label>
-                        <input type="text" className="form-control" name="Sort Code" value={this.state.account.sortCode}
-                               onChange={(e) => this.setSortCode(e)}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Account Number:</label>
-                        <input type="text" className="form-control" name="Account Number"
-                               value={this.state.account.accountNumber}
-                               onChange={(e) => this.setAccountNumber(e)}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Bank Name:</label>
-                        <select value={this.state.account.accountBank} onChange={this.handleAccountBankChange}
-                                className="form-control">
-                            <option value="">Please select account bank name</option>
-                            {this.state.bankNames.map((type) => (
-                                <option value={type} key={type}>{type}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label>Currency:</label>
-                        <select value={this.state.account.currency} onChange={this.handleAccountCurrencyChange}
-                                className="form-control">
-                            <option value="">Please select account currency</option>
-                            {this.state.currencies.map((type) => (
-                                <option value={type} key={type}>{type}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label>Balance:</label>
-                        <input type="number" className="form-control" name="Current Balance"
-                               value={this.state.account.currentBalance}
-                               onChange={(e) => this.setCurrentBalance(e)}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Balance Date:</label>
-                        <input type="date" className="form-control" name="Balance Date:"
-                               value={this.state.account.balanceDate}
-                               onChange={(e) => this.setCurrentBalanceDate(e)}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Is this your main account?</label>
-                        <input type="checkbox" defaultChecked name="Main Account"
-                               value={this.state.account.isMainAccount}
-                               onChange={(e) => this.setIsMainAccount(e)}/>
-                    </div>
-                    <div className="form-buttons">
-                        <input type="reset" className="btn btn-danger" value="Cancel"/>
-                        <input type="submit" className="btn btn-primary" value="Save"/>
-                    </div>
-                </form>
+                <Form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalAccountName">
+                        <Form.Label column sm={4}>Account Name:</Form.Label>
+                        <Col sm={8}>
+                            <input type="text" className="form-control" name="accountName"
+                                   value={this.state.account.name}
+                                   onChange={(e) => this.setAccountName(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalAccountType">
+                        <Form.Label column sm={4}>Account Type:</Form.Label>
+                        <Col sm={8}>
+                            <select className="form-control" value={this.state.account.accountType}
+                                    onChange={this.handleAccountTypeChange}>
+                                <option value="">Please select account type</option>
+                                {this.state.accountTypes.map((type) => (
+                                    <option value={type} key={type}>{type}</option>
+                                ))}
+                            </select>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalSortCode">
+                        <Form.Label column sm={4}>Sort Code:</Form.Label>
+                        <Col sm={8}>
+                            <input type="text" className="form-control" name="Sort Code"
+                                   value={this.state.account.sortCode}
+                                   onChange={(e) => this.setSortCode(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalAccountNumber">
+                        <Form.Label column sm={4}>Account Number:</Form.Label>
+                        <Col sm={8}>
+                            <input type="text" className="form-control" name="Account Number"
+                                   value={this.state.account.accountNumber}
+                                   onChange={(e) => this.setAccountNumber(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalBankName">
+                        <Form.Label column sm={4}>Bank Name:</Form.Label>
+                        <Col sm={8}>
+                            <select value={this.state.account.accountBank} onChange={this.handleAccountBankChange}
+                                    className="form-control">
+                                <option value="">Please select account bank name</option>
+                                {this.state.bankNames.map((type) => (
+                                    <option value={type} key={type}>{type}</option>
+                                ))}
+                            </select>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalCurrency">
+                        <Form.Label column sm={4}>Currency:</Form.Label>
+                        <Col sm={8}>
+                            <select value={this.state.account.currency} onChange={this.handleAccountCurrencyChange}
+                                    className="form-control">
+                                <option value="">Please select account currency</option>
+                                {this.state.currencies.map((type) => (
+                                    <option value={type} key={type}>{type}</option>
+                                ))}
+                            </select>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalBalance">
+                        <Form.Label column sm={4}>Balance:</Form.Label>
+                        <Col sm={8}>
+                            <input type="number" className="form-control" name="Current Balance"
+                                   value={this.state.account.currentBalance}
+                                   onChange={(e) => this.setCurrentBalance(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalBalanceDate">
+                        <Form.Label column sm={4}>Balance Date:</Form.Label>
+                        <Col sm={8}>
+                            <input type="date" className="form-control" name="Balance Date:"
+                                   value={this.state.account.balanceDate}
+                                   onChange={(e) => this.setCurrentBalanceDate(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalMainAccount">
+                        <Form.Label column sm={4}>Is this your main account?</Form.Label>
+                        <Col sm={8}>
+                            <input type="checkbox" defaultChecked name="Main Account"
+                                   value={this.state.account.isMainAccount}
+                                   onChange={(e) => this.setIsMainAccount(e)}/>
+                        </Col>
+                    </Form.Group>
+                    <FormGroup className="form-buttons">
+                        <Button type="reset" className="btn btn-danger">Cancel</Button>
+                        <Button type="submit" className="btn btn-primary">Save</Button>
+                    </FormGroup>
+                </Form>
             </div>
         )
     }
