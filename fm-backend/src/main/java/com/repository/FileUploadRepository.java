24921743749d5e2b9c087ca3da162218f48ba28c
Lydia Adejumo <lydiaadejumo@outlook.com>
2025-03-12 11:48:38 +0000

    [FM-57] Limit the data retrieved for upload history page

--- fm-backend/src/main/java/com/repository/FileUploadRepository.java
+++ fm-backend/src/main/java/com/repository/FileUploadRepository.java
@@ -1,9 +1,15 @@
 package com.repository;
 
 import com.dto.FileUpload;
+import com.dto.response.FileInfoResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
+    @Query("SELECT new com.dto.response.FileInfoResponse(f.id, f.fileName, f.successfulTransactions, f.failedTransactions, f.uploadedAt) FROM FileUpload f")
+    List<FileInfoResponse> getUploadCondensedData();
 }
